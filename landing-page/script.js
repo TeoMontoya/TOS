d876095c6fb4b523e8e01f3f201fdbea6fd7db98
Teo <mateomontoya-97@outlook.com>
2023-05-18 18:48:38 -0400

    finished css, missing only the img placeholder, but will leave it like that lol

---
+++ landing-page/script.js
@@ -0,0 +1,5 @@
+function handleImageError(image) {
+    image.style.display = 'none'; // Hide the broken image
+    // Alternatively, you can replace the broken image with a placeholder
+    // image.src = 'path/to/placeholder.jpg';
+  }
